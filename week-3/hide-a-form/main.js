ebfd0536feadd9f3498f7d791e24e4e91a85ea22
Abrar Satar <aj_satar@hotmail.com>
2016-07-13 22:34:05 +0000

    finished form

---
+++ week-3/hide-a-form/main.js
@@ -0,0 +1,51 @@
+angular.module("myApp", [ ])
+
+angular.module("myApp")
+    .controller("alphaCtrl", alphaCtrl)
+    
+
+function alphaCtrl() {
+    
+    var alpha = this
+    
+    alpha.myObject = { }
+    
+    alpha.myObject.Name = "person"
+    alpha.myObject.bio = "my bio dasfjadsflk ;jds  dafdsf sdaf dsfa "
+    alpha.myObject.fBooks = "my fave books dsfadsf dsaf dsaf "
+    alpha.myObject.faveJS = "my faveJS dsafadsf dasf adsf "    
+    
+    
+    alpha.change = true
+    
+    alpha.newObject = { }
+    
+    alpha.newObject.Name = ""
+    alpha.newObject.bio = ""
+    alpha.newObject.fBooks = ""
+    alpha.newObject.faveJS = ""
+   
+   
+    alpha.editClick = function (){
+       
+       alpha.change = !alpha.change;
+    
+    }
+    
+    alpha.newText = function (){
+       
+    alpha.change = !alpha.change;
+    alpha.myObject.Name=alpha.newObject.Name;
+     alpha.myObject.bio=alpha.newObject.bio;
+      alpha.myObject.fBooks=alpha.newObject.fBooks;
+       alpha.myObject.faveJS=alpha.newObject.faveJS;
+    console.log()
+    
+        
+        
+    }
+    
+    
+    
+    
+}
