9a6824861e10f32ba2068b568c48fcfd55892b4e
Abrar Satar <aj_satar@hotmail.com>
2016-07-19 16:43:25 +0000

    new stuff

--- warm-up/main.js
+++ warm-up/main.js
@@ -1,91 +1,11 @@
-angular.module("CakeApp", [ ])
-    .controller("menuController" , 
-    ['CheesecakeFactory',
-    menuController])
-    
-    
-    .controller("menuOrderController" ,
-        [ 
-        'CheesecakeFactory',
-        menuOrderController])
-        
-        
-        
-    .factory('CheesecakeFactory', CheesecakeFactory)
-
-
-function menuController(CheesecakeFactory) {
-   
-   console.log("menuController is loaded")
-    
-    var menu = this
-    menu.items = CheesecakeFactory.menuItems;
-    menu.title = 'Awesome Sauce'
-    
-    menu.lowCalories = function (menuItem) {
-    return menuItem.calories < 2000    
-    }
-    
-    
-}    
-
-
-
-
-function menuOrderController(CheesecakeFactory){
- console.log("menuOrderController is loaded", CheesecakeFactory)
-    var menu = this
-    menu.items = CheesecakeFactory.menuItems;
-
-    
+function House (size, bedrooms, stories, bathrooms) {
+    this.size = size;
+    this.bedrooms = bedrooms;
+    this.stories = stories;
+    this.bathrooms = bathrooms;
 }
 
 
+var myHome = new House (3000, 5, 3, 3)
 
-
-
-
-
-function CheesecakeFactory () {
-    
-    console.log("factory is loaded")
-    
-    var menuItems = [
-        {
-        name: 'Egg Roll',
-        calories: 6000,
-        deliciousness: 3,
-        flavor: 'southwestern',
-        price: 50
-       
-        },
-          {
-        name: 'strawberry cheesecake',
-        calories: 1500,
-        deliciousness: 9,
-        flavor: 'strawberry',
-        price: 9.95 
-
-        },
-          {
-        name: 'Turtle Cheesecake',
-        calories: 1800 ,
-        deliciousness: 10,
-        flavor: 'Chocolate Turtle',
-        price: 13,
-        }
-        ]
-    
-    
-    
-    var MenuTitle = "Lunch & Dinner"
-    
-    return {
-        menuItems: menuItems,
-        title: MenuTitle,
-        
-        
-    }
-        
-    
-}
+var yourHome = new House (800, 1, 1, 1)
