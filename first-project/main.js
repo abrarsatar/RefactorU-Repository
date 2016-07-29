a610b7a497c966fe0f918234e9c438908e4d5403
Abrar Satar <aj_satar@hotmail.com>
2016-07-29 16:08:48 +0000

    push midterm

--- first-project/main.js
+++ first-project/main.js
@@ -1,8 +1,40 @@
-document.getElementById("signin").onclick = function() {
-    // document.querySelector(".entirebody").style.display = "none";
-    document.querySelector("#signindiv").style.display = "block";
+angular.module('myApp', [ ])
 
+angular.module('myApp')
+    .controller('alphaController', alphaController )
     
+    
+    
+function alphaController ( ) {
+  console.log('alpha is working')
+  
+  
+
+  var alpha = this
+  // alpha.home = true;
+  // alpha.profile = false;
+  alpha.myValue = 10;
+  
+  alpha.slider = {
+    value: 10
+};
+  
+  alpha.homeClick = function () {
+    alpha.home = true;
+    alpha.profile = false;
+    
+  }
+  
+    
+  alpha.profileClick = function () {
+    alpha.home = false;
+    alpha.profile = true;
+    
+  }
+  
 }
+
+
+
 
 
